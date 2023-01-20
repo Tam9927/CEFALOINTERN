1024161c5cc315c5a7b478d6d69023ae8f1e5f9b
Tam9927 <55223962+Tam9927@users.noreply.github.com>
2023-01-20 18:36:20 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+class Calculator{
+
+clear()
+
+append()
+
+compute()
+
+update()
+
+
+
+
+
+}
+
+
+const numberButtons=document.querySelectorAll('[data-number]')
+const operationButtons=document.querySelectorAll('[data-operation]')
+const equalsButton=document.querySelector('[data-equals]')
+const deleteButton=document.querySelector('[data-delete]')
+const allClearButton=document.querySelector('[data-all-clear]')
+const previousOperandTextElement=document.querySelectorAll('[data-previous-operand]')
+const currentOperandTextElement=document.querySelectorAll('[data-current-operand]')
